e4ef5ce4438ec8a4324fe1296e9edebfeb6ce485
linbendatarho <linben@datarho.tech>
2023-12-24 16:58:50 +0800

    add turtles test

--- iturtle/tests/test_widget.py
+++ iturtle/tests/test_widget.py
@@ -4,7 +4,7 @@ Test cases for turtle creation.
 
 import pytest
 
-from ..turtle import Turtle
+from ..turtle import Turtle, Screen
 
 
 def test_turtle_creation():
@@ -22,19 +22,19 @@ def test_turtle_forward():
     """
     turtle = Turtle()
 
-    assert turtle.x == Turtle.WIDTH / 2
-    assert turtle.y == Turtle.HEIGHT / 2
+    assert turtle.x == Screen.WIDTH / 2
+    assert turtle.y == Screen.HEIGHT / 2
 
     turtle.forward(80)
 
-    assert turtle.x == Turtle.WIDTH / 2 + 80
-    assert turtle.y == Turtle.HEIGHT / 2
+    assert turtle.x == Screen.WIDTH / 2 + 80
+    assert turtle.y == Screen.HEIGHT / 2
 
     turtle.right(90)
     turtle.forward(80)
 
-    assert turtle.x == Turtle.WIDTH / 2 + 80
-    assert turtle.y == Turtle.HEIGHT / 2 + 80
+    assert turtle.x == Screen.WIDTH / 2 + 80
+    assert turtle.y == Screen.HEIGHT / 2 + 80
 
 
 def test_turtle_backward():
@@ -43,19 +43,19 @@ def test_turtle_backward():
     """
     turtle = Turtle()
 
-    assert turtle.x == Turtle.WIDTH / 2
-    assert turtle.y == Turtle.HEIGHT / 2
+    assert turtle.x == Screen.WIDTH / 2
+    assert turtle.y == Screen.HEIGHT / 2
 
     turtle.backward(80)
 
-    assert turtle.x == Turtle.WIDTH / 2 - 80
-    assert turtle.y == Turtle.HEIGHT / 2
+    assert turtle.x == Screen.WIDTH / 2 - 80
+    assert turtle.y == Screen.HEIGHT / 2
 
     turtle.right(90)
     turtle.backward(80)
 
-    assert turtle.x == Turtle.WIDTH / 2 - 80
-    assert turtle.y == Turtle.HEIGHT / 2 - 80
+    assert turtle.x == Screen.WIDTH / 2 - 80
+    assert turtle.y == Screen.HEIGHT / 2 - 80
 
 
 def test_turtle_right():
@@ -101,16 +101,16 @@ def test_turtle_goto():
     turtle.forward(10)
     turtle.goto(0, 0)
 
-    assert turtle.x == Turtle.WIDTH / 2
-    assert turtle.y == Turtle.HEIGHT / 2
+    assert turtle.x == Screen.WIDTH / 2
+    assert turtle.y == Screen.HEIGHT / 2
 
     turtle = Turtle()
 
     turtle.forward(10)
     turtle.goto((0, 0))
 
-    assert turtle.x == Turtle.WIDTH / 2
-    assert turtle.y == Turtle.HEIGHT / 2
+    assert turtle.x == Screen.WIDTH / 2
+    assert turtle.y == Screen.HEIGHT / 2
 
 
 def test_turtle_teleport():
@@ -121,8 +121,8 @@ def test_turtle_teleport():
     turtle.forward(10)
     turtle.teleport(0, 0)
 
-    assert turtle.x == Turtle.WIDTH / 2
-    assert turtle.y == Turtle.HEIGHT / 2
+    assert turtle.x == Screen.WIDTH / 2
+    assert turtle.y == Screen.HEIGHT / 2
 
 
 def test_turtle_dot():
@@ -133,21 +133,21 @@ def test_turtle_dot():
 
     turtle.dot()
 
-    assert turtle.action["size"] == 5
+    assert turtle.screen.action["size"] == 5
 
     turtle.dot(50)
 
-    assert turtle.action["size"] == 50
+    assert turtle.screen.action["size"] == 50
 
     turtle.dot(50, "blue")
 
-    assert turtle.action["color"] == "blue"
-    assert turtle.action["size"] == 50
+    assert turtle.screen.action["color"] == "blue"
+    assert turtle.screen.action["size"] == 50
 
     turtle.dot(50, (255, 0, 0))
 
-    assert turtle.action["color"] == "#ff0000"
-    assert turtle.action["size"] == 50
+    assert turtle.screen.action["color"] == "#ff0000"
+    assert turtle.screen.action["size"] == 50
 
 
 def test_turtle_towards():
@@ -160,8 +160,8 @@ def test_turtle_towards():
     turtle.setheading(turtle.towards(0, 0))
     turtle.fd(10)
 
-    assert turtle.x == Turtle.WIDTH / 2
-    assert turtle.y == Turtle.HEIGHT / 2
+    assert turtle.x == Screen.WIDTH / 2
+    assert turtle.y == Screen.HEIGHT / 2
     assert abs(turtle.pos()[0]) < 1e-5
     assert abs(turtle.pos()[1]) < 1e-5
 
@@ -171,8 +171,8 @@ def test_turtle_towards():
     turtle.setheading(turtle.towards((0, 0)))
     turtle.fd(10)
 
-    assert turtle.x == Turtle.WIDTH / 2
-    assert turtle.y == Turtle.HEIGHT / 2
+    assert turtle.x == Screen.WIDTH / 2
+    assert turtle.y == Screen.HEIGHT / 2
     assert abs(turtle.pos()[0]) < 1e-5
     assert abs(turtle.pos()[1]) < 1e-5
 
@@ -185,24 +185,24 @@ def test_turtle_write():
 
     turtle.write("hello world")
 
-    assert turtle.action["text"] == "hello world"
-    assert turtle.action["align"] == "left"
-    assert turtle.action["font"] == ("Arial", 8, "normal")
+    assert turtle.screen.action["text"] == "hello world"
+    assert turtle.screen.action["align"] == "left"
+    assert turtle.screen.action["font"] == ("Arial", 8, "normal")
 
     turtle = Turtle()
 
     turtle.write("hello world", "right")
 
-    assert turtle.action["text"] == "hello world"
-    assert turtle.action["align"] == "right"
-    assert turtle.action["font"] == ("Arial", 8, "normal")
+    assert turtle.screen.action["text"] == "hello world"
+    assert turtle.screen.action["align"] == "right"
+    assert turtle.screen.action["font"] == ("Arial", 8, "normal")
 
     turtle = Turtle()
     turtle.write("hello", font=("Courier", 90, "normal"), align="center")
 
-    assert turtle.action["text"] == "hello"
-    assert turtle.action["font"] == ("Courier", 90, "normal")
-    assert turtle.action["align"] == "center"
+    assert turtle.screen.action["text"] == "hello"
+    assert turtle.screen.action["font"] == ("Courier", 90, "normal")
+    assert turtle.screen.action["align"] == "center"
 
 
 def test_turtle_circle():
@@ -216,3 +216,24 @@ def test_turtle_circle():
     assert abs(turtle.pos()[0] + 100) < 1e-5
     assert abs(turtle.pos()[1] - 240) < 1e-5
     assert abs(turtle.heading() + 540) < 1e-5
+
+
+def test_multi_turtles():
+    s = Screen()
+
+    A = Turtle(s)
+    assert A.x == Screen.WIDTH / 2
+    assert A.y == Screen.HEIGHT / 2
+
+    B = Turtle(s)
+    assert B.x == Screen.WIDTH / 2
+    assert B.y == Screen.HEIGHT / 2
+
+    A.forward(80)
+    assert A.x == Screen.WIDTH / 2 + 80
+    assert A.y == Screen.HEIGHT / 2
+
+    B.right(90)
+    B.forward(80)
+    assert B.x == Screen.WIDTH / 2
+    assert B.y == Screen.HEIGHT / 2 + 80
